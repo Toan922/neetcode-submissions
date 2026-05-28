961617c2f2c18186675ff825c2862ac86288ff28
Toan Nguyen <khanhtoannguyen.004@gmail.com>
2026-05-28 00:28:52 -0500

    Add: top-k-elements-in-list - submission-9

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-9.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        //init a counter hash that will serve as a key
+        unordered_map<int, int> counter;
+        /*
+            init a freq vector which contains vectors
+            make the size of nums + 1 to ensure for edge cases where an elem can be the only elem in num
+        */
+        vector<vector<int>> freq(nums.size() + 1);
+        //init the res vector that we need to return
+        vector<int> res;
+
+        //loop thru nums and count the number of each elem's occurence
+        for(int n : nums)
+        {
+            //increment the count of the element
+            counter[n]++;
+        }
+
+        //loop through counter grabbing each number and its count as a key value pair
+        for(auto& [key, value] : counter)
+        {
+            //use the value/count as the index for freq and the key/number as what we need to append
+            freq[value].push_back(key);
+        }
+
+        //iterate backwards through freq from n occurences to 1 occurence
+        for(int i = freq.size() - 1; i > 0; i--)
+        {
+            //for each row of freq keep appending vals to res until res' length == k
+            for(int j = 0; j < freq[i].size(); j++)
+            {
+                res.push_back(freq[i][j]);
+
+                //check if size of res is equal to k -> return res if true
+                if(res.size() == k)
+                {
+                    return res;
+                }
+            }
+        }
+    }
+};
